921c6f960d2327019c3e894bbc460ae5c6c2b4eb
k-r-3 <rke19922802@gmail.com>
2020-12-16 13:44:49 +0500

    Разбор первой программы. [#433622]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Kozlov");
+        System.out.println("28.02.1992");
+    }
+}
