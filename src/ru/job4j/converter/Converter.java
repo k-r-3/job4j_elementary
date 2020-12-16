118a034884f1307e15f1885eff13926e29f9d9d8
k-r-3 <rke19922802@gmail.com>
2020-12-16 20:52:38 +0500

    Конвертер валюты [#433601]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -14,8 +14,8 @@ public class Converter{
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
         int dollar = Converter.rubleToDollar(120);
+        System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
