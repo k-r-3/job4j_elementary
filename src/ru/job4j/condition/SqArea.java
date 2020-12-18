5d50547bcf6f7964547a08c6af1062df1bab0f94
k-r-3 <rke19922802@gmail.com>
2020-12-18 19:41:22 +0500

    Тесты для стороны прямоугольника. [#433638]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -11,5 +11,11 @@ public class SqArea {
     public static void main(String[] args) {
         double result = SqArea.square(6.0, 2.0);
         System.out.println(" p = 6.0, k = 2.0, s = 2.0, real = " + result);
+        result = SqArea.square(4.0, 1.0);
+        System.out.println(" p = 4.0, k = 1.0, s = 1.0, real = " + result);
+        result = SqArea.square(9.0, 4.0);
+        System.out.println(" p = 9.0, k = 4.0, s = 3.24, real = " + result);
+        result = SqArea.square(5.0, 4.0);
+        System.out.println(" p = 5.0, k = 4.0, s = 1.0, real = " + result);
     }
 }
---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP4K1Then1() {
+        int expected = 1,
+                p = 4,
+                k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP9K4Then3and24() {
+        double expected = 3.24,
+                p = 9.0,
+                k = 4.0;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP5K4Then1() {
+        int expected = 1,
+                p = 5,
+                k = 4;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
