abd0998c7b893c169eda90ece01a53208d1d5815
k-r-3 <rke19922802@gmail.com>
2020-12-19 16:53:06 +0500

    Глупый бот [#433545]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -4,10 +4,8 @@ public class DummyBot {
 
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        if ("Привет, Бот".equals(question)) {
+        if ("Привет, Бот.".equals(question)) {
             rsl =  "Привет, умник.";
-        } else if ("Сколько будет 2 + 2?".equals(question)) {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
