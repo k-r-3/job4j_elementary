9d97580006e96bd09121747e00c696cdbe6fee49
k-r-3 <rke19922802@gmail.com>
2020-12-19 17:16:45 +0500

    Множественное логическое выражение И [#433565]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -10,4 +10,10 @@ public class TriangleTest {
         boolean result = Triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
+
+    @Test
+    public void whenNotExist() {
+        boolean result = Triangle.exist(1.0, 0.5, 2.0);
+        assertThat(result, is(true));
+    }
 }
