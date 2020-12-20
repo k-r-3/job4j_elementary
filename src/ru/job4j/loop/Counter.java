602941267c96d173e53b60356b146d65d355cbed
k-r-3 <rke19922802@gmail.com>
2020-12-20 16:05:45 +0500

    Сумма чисел. [#433664]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -4,17 +4,21 @@ public class Counter {
 
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (start = 0; start < finish; start++) {
-            sum += start;
+        for (int i = start; i <= finish; i++) {
+            sum += i;
         }
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(10, 21));
-        System.out.println(sum(2, 20));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            } else {
+                sum += 0;
+            }
+        }
+        return sum;
     }
 }
