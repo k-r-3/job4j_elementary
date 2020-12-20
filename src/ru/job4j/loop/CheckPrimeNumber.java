f4dacd37c05d8d0a944ff17c3421e67426dd6348
k-r-3 <rke19922802@gmail.com>
2020-12-20 17:39:58 +0500

    Простое число [#433662]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,4 +1,4 @@
-package ru.job4j.loop.ru.job4j.loop;
+package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
--- test/ru/job4j/loop/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop.ru.job4j.loop;
 
 import org.junit.Test;
+import ru.job4j.loop.CheckPrimeNumber;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
