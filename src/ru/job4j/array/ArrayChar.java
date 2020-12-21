d86630038d753fd51a4713db3a07578400994041
k-r-3 <rke19922802@gmail.com>
2020-12-21 22:26:41 +0500

    Слово начинается с ... [#433571]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        int size = Math.min(word.length, pref.length);
+        for (int i = 0; i < size; i++) {
+            result = word[i] == pref[i];
+            if (!result) {
+                break;
+            }
+        }
+        return result;
+    }
+}
