3a5817c9461ea126e4804e50d3226fe1b8e823f1
k-r-3 <rke19922802@gmail.com>
2020-12-22 13:26:12 +0500

    Массив заполнен true или false [#433572]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            result = data[1] == data[i];
+        for (int i = 1; i < data.length; i++) {
+            result = data[0] == data[i];
             if (!result) {
                 break;
             }
