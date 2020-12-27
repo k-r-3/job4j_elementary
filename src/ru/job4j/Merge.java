ec115c4f71678e35936bc565c60574b891b5dfb1
k-r-3 <rke19922802@gmail.com>
2020-12-27 16:38:45 +0500

    Экзамен.Объединить два отсортированных массива

--- src/ru/job4j/Merge.java
+++ src/ru/job4j/Merge.java
@@ -3,14 +3,29 @@ package ru.job4j;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        System.arraycopy(left, 0, rsl, 0, left.length);
-        System.arraycopy(right, 0, rsl, left.length, right.length);
-        for (int i = 1; i < rsl.length; i++) {
-            if (rsl[i] < rsl[i - 1]) {
-                int temp = rsl[i - 1];
-                rsl[i - 1] = rsl[i];
-                rsl[i] = temp;
+        int k = 0,
+                i = 0,
+                j = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                rsl[k] = left[i];
+                i++;
+            } else {
+                rsl[k] = right[j];
+                j++;
             }
+            k++;
+        }
+        while (i < left.length) {
+            rsl[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            rsl[k] = right[j];
+            j++;
+            k++;
         }
         return rsl;
     }
